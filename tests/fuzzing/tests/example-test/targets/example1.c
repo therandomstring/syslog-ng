bdc6c19fb5ebc49f0fc5cacc0964aacedce4111c
Bálint Horváth <bal.horv.98@gmail.com>
2023-01-05 14:53:34 +0100

    fixup! WIP fuzzing: fuzz test
    Signed-off-by: Bálint Horváth <bal.horv.98@gmail.com>

--- tests/fuzzing/tests/example-test/targets/example1.c
+++ tests/fuzzing/tests/example-test/targets/example1.c
@@ -1,13 +1,35 @@
+/*
+ * Copyright (c) 2023 One Identity LLC
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
 #include "examples/destinations/example_destination/example_destination.h"
 #include "examples/destinations/example_destination/example_destination_worker.h"
 #include "msg-format.h"
 #include "fuzzing_helper.h"
 
-//FIXME: secret storage init failss
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size){
   if (size <= 1) return 0;
 
-  AppInfo *app = app_init("example-destination");
+  AppInfo *app = app_new();
+  app->load_module(app, "example-test");
 
   //TODO: maybe this also can be a helper macro/template
   ExampleDestinationDriver *dd = (ExampleDestinationDriver *) example_destination_dd_new(app->config);
@@ -22,6 +44,6 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size){
   log_threaded_dest_worker_disconnect((LogThreadedDestWorker *) dw);
 
   syslog_message_free(message);
-  app_deinit(app);
+  app_free(app);
 	return 0;
 }
